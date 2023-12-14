89785cadbc9d5f7b1ceae14672e41b12c44b1397
jaydrennan <jsdman1313@gmail.com>
2023-12-13 22:54:38 -0800

    refactor: adds missing periods.

--- src/routes/learning-resources/+page.svelte
+++ src/routes/learning-resources/+page.svelte
@@ -12,9 +12,9 @@
         </tr>
       </thead>
       <tbody>
-        <tr><td><a href="https://learnpythonthehardway.org/">Learn Python The Hard Way</a></td><td>Start here for learning Python basics</td></tr>
-        <tr><td><a href="https://effectivepython.com/">Effective Python: 90 Specific Ways to Write Better Python</a></td><td>Beyond the basics, python tips</td></tr>
-        <tr><td><a href="https://github.com/satwikkansal/wtfpython">wtfpython</a></td><td>Learn how python works under the hood through snippets with surpising behaviour</td></tr>
+        <tr><td><a href="https://learnpythonthehardway.org/">Learn Python The Hard Way</a></td><td>Start here for learning Python basics.</td></tr>
+        <tr><td><a href="https://effectivepython.com/">Effective Python: 90 Specific Ways to Write Better Python</a></td><td>Beyond the basics, python tips.</td></tr>
+        <tr><td><a href="https://github.com/satwikkansal/wtfpython">wtfpython</a></td><td>Learn how python works under the hood through snippets with surpising behaviour.</td></tr>
 
       </tbody>
       <thead>
@@ -24,7 +24,7 @@
       </thead>
       <tbody>
         <tr><td><a href="https://www.fast.ai/">FastAI</a></td><td>Make's creating AI fast and easy.  Great place to start learning. Free textbook or youtube course.</td></tr>
-        <tr><td><a href="https://d2l.ai/index.html">Dive into Deep Learning</a></td><td>Interactive deep learning book with code, math, and discussions. Personally find it very approachable</td></tr>
+        <tr><td><a href="https://d2l.ai/index.html">Dive into Deep Learning</a></td><td>Interactive deep learning book with code, math, and discussions. Personally find it very approachable.</td></tr>
       </tbody>
       <thead>
         <tr>
@@ -32,7 +32,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr><td><a href="https://pythonspeed.com/docker/">pythonspeed.com</a></td><td>Basics and beyond for production quality docker containerization with an emphasis on Python</td></tr>
+        <tr><td><a href="https://pythonspeed.com/docker/">pythonspeed.com</a></td><td>Basics and beyond for production quality docker containerization with an emphasis on Python.</td></tr>
       </tbody>
       <thead>
         <tr>
@@ -48,7 +48,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr><td><a href="https://www.oreilly.com/library/view/python-for-devops/9781492057680/">Python for Devops</a></td><td>Overview of different automation tools for Devops, with emphasis on Python</td></tr>
+        <tr><td><a href="https://www.oreilly.com/library/view/python-for-devops/9781492057680/">Python for Devops</a></td><td>Overview of different automation tools for Devops, with emphasis on Python.</td></tr>
       </tbody>
       <thead>
         <tr>
